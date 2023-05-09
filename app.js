a35c6779761ba2850d405f37d03246652175b8bf
HerosReis <heros.reis@gmail.com>
2023-05-09 15:24:18 +0000

    Mais uma alteracao

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ console.log("aplicação está se conectando ao banco de dados")
 // mais um console log de teste
 console.log("O paulo fez esse commit sem voce ver na aula")
 console.log("enviando para o GitHub")
+console.log("Fazendo mais uma alteracao")
 
